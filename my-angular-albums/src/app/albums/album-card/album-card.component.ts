7fe424dd18d85b45d74ca6dc50449570d4c4db37
Joe Miller <joseph.miller@ssa.gov>
2019-09-25 11:45:26 -0400

    after chapter 4

---
+++ my-angular-albums/src/app/albums/album-card/album-card.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Album } from '../album.model';
+
+@Component({
+  selector: 'app-album-card',
+  templateUrl: './album-card.component.html',
+  styleUrls: ['./album-card.component.css']
+})
+export class AlbumCardComponent implements OnInit {
+
+  @Input()
+  album: Album;
+
+  @Output()
+ albumClicked: EventEmitter<Album> = new EventEmitter<Album>();
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  showAlbum() {
+    this.albumClicked.emit(this.album);
+  }
+
+  showArtist() {
+    console.log('Artist selected: ' + this.album.artist);
+  }
+
+}
